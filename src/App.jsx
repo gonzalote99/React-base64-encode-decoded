a4daacfdc248378ddb905bffa062c9d748e6c16f
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2024-01-19 16:49:57 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import {useEffect, useState} from 'react';
+
+function App() {
+  const [input, setInput] = useState('');
+
+
+  function Encode() {
+      const input = document.getElementById('input').value;
+      const output = window.btoa(input);
+      document.getElementById('output').value = output;
+    }
+
+    function Decode() {
+      const input = document.getElementById('input').value;
+      const output = window.atob(input);
+      document.getElementById('output').value = output;
+    }
+
+  return(
+    <div className='flex items-center justify-center bg-gray-100 h-screen'>
+    <div className='flex-col w-11/12 sm:w-10/12'>
+    <div className='sm:full m-2'>
+    <p className='text-2xl font-bold'>base64 encoder/decoder</p>
+      <p className=''>BASE64</p>
+    </div>
+      <div className='flex flex-col lg:flex-row'>
+      <div className='w-full lg:w-1/2 px-2 pt-2 sm:pt-2 pb-2'>
+      <textarea id='input' className='h-48 w-full border p-2 border-gray-200' placeholder='input' />
+      </div>
+        <div className='w-full lg:w-1/2 px-2 pt-0 sm:pt-2 pb-2'>
+        <textarea id='output' className='h-48 w-full border p-0 border-gray-200' placeholder='output' />
+        </div>
+
+      </div>
+      <div className='sm:full'>
+      <button className='my-2 mx-2 bg-gray-200 px-3 py-2' onClick={() => Encode()}>Encode</button>
+        <button className='my-2 bg-gray-200 px-3 py-2' onClick={() => Decode()}>Decode</button>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
